fc2f0032d024175706589a84df8ff9ee3ea0a547
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2020-08-30 15:53:02 -0500

    Work on home page

--- application/networking/routes.py
+++ application/networking/routes.py
@@ -1,12 +1,11 @@
 import logging
 
+import flask
 from flask import current_app, redirect, render_template, request
 
 from application import GAME_MANAGER_CONFIG_KEY
 from application.data.game_manager import GameManager
-from application.data.scoring_type import ScoringType
 from . import main
-
 
 LOG = logging.getLogger("Routes")
 
@@ -21,27 +20,28 @@ def game_page(game_name: str):
     game_state = _get_game_manager().get_game_state(game_name)
 
     if game_state:
-        return render_template("game.html", game_state=game_state)
+        player_name = game_state.player_ids_to_names[_get_player_id()]
+        return render_template("game.html", game_state=game_state, player_name=player_name)
     return "Could not find game!", 404
 
 
 @main.route("/create_game", methods=["POST"])
 def create_game():
-    scoring_type = None
+    player_id = _get_player_id()
+    LOG.info(f"Creating game for {player_id}")
+
+    player_name = player_id
     if request.form:
-        scoring_type_string: str = request.form.get("scoring-type", "classic")
-        if "(fractional)" in scoring_type_string.lower():
-            scoring_type = ScoringType.DISTRIBUTED_FRACTIONAL
-        elif "(integer)" in scoring_type_string.lower():
-            scoring_type = ScoringType.DISTRIBUTED_INTEGER
-        else:
-            scoring_type = ScoringType.CLASSIC
+        player_name = request.form.get("player_name", player_id)
 
-    LOG.info(f"Creating game with scoring type {scoring_type}")
-
-    game_state = _get_game_manager().create_game(scoring_type)
+    game_state = _get_game_manager().create_game()
+    game_state.new_player(player_id, player_name)
     return redirect(f"/games/{game_state.game_name}", code=302)
 
 
 def _get_game_manager() -> GameManager:
     return current_app.config[GAME_MANAGER_CONFIG_KEY]
+
+
+def _get_player_id() -> str:
+    return flask.request.remote_addr
